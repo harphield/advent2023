19f27421733d318b8ed05963b9a873e3ae5079d9
harphield <harphield@gmail.com>
2023-12-06 11:02:36 +0100

    Day 6 part 2 done

--- a06/src/main.rs
+++ a06/src/main.rs
@@ -1,7 +1,8 @@
+use regex::Regex;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
-use regex::Regex;
+use std::ops::Add;
 
 fn main() -> Result<(), io::Error> {
     let file = File::open("input.txt")?;
@@ -13,22 +14,42 @@ fn main() -> Result<(), io::Error> {
     let mut times = vec![];
     let mut distances = vec![];
 
+    let mut part2time = 0;
+    let mut part2distance = 0;
+
     for line_r in io::BufReader::new(file).lines() {
         match line_r {
             Ok(line) => {
-                let numbers = re.find_iter(&line).map(|m| {
-                    m.as_str().parse::<u32>().unwrap()
-                }).collect::<Vec<u32>>();
+                let numbers = re
+                    .find_iter(&line)
+                    .map(|m| m.as_str().parse::<u64>().unwrap())
+                    .collect::<Vec<u64>>();
 
                 if line_nr == 0 {
                     // times
                     times = numbers;
+
+                    part2time = re
+                        .find_iter(&line)
+                        .map(|m| m.as_str().to_string())
+                        .reduce(|acc, e| acc.add(e.as_str()))
+                        .unwrap()
+                        .parse::<u64>()
+                        .unwrap();
                 } else {
                     // distances
                     distances = numbers;
+
+                    part2distance = re
+                        .find_iter(&line)
+                        .map(|m| m.as_str().to_string())
+                        .reduce(|acc, e| acc.add(e.as_str()))
+                        .unwrap()
+                        .parse::<u64>()
+                        .unwrap();
                 }
             }
-            Err(_) => break
+            Err(_) => break,
         }
 
         line_nr += 1;
@@ -39,17 +60,20 @@ fn main() -> Result<(), io::Error> {
     times.iter().enumerate().for_each(|(i, time)| {
         let distance = distances.get(i).unwrap();
 
-        let found = find_record_times(time, distance);
-        mul *= found.len();
+        mul *= find_record_times(time, distance);
     });
 
     println!("Part 1 result: {}", mul);
 
+    let found = find_record_times(&part2time, &part2distance);
+
+    println!("Part 2 result: {}", found);
+
     Ok(())
 }
 
-fn find_record_times(race_time: &u32, min_distance: &u32) -> Vec<u32> {
-    let mut found = vec![];
+fn find_record_times(race_time: &u64, min_distance: &u64) -> u64 {
+    let mut found = 0;
 
     // distance = time_held * time_traveled
     // race_time = time_held + time_traveled
@@ -64,13 +88,13 @@ fn find_record_times(race_time: &u32, min_distance: &u32) -> Vec<u32> {
     // x1 = (race_time + sqrt(D)) / 2
     // x2 = (race_time - sqrt(D)) / 2
 
-    let d = race_time.pow(2) as f32 - 4f32 * *min_distance as f32;
-    let at_most = (*race_time as f32 + d.sqrt()) / 2f32;
-    let at_least = (*race_time as f32 - d.sqrt()) / 2f32;
+    let d = (*race_time as f64).powf(2f64) as f64 - 4f64 * *min_distance as f64;
+    let at_most = (*race_time as f64 + d.sqrt()) / 2f64;
+    let at_least = (*race_time as f64 - d.sqrt()) / 2f64;
 
-    for hold in at_least.ceil() as u32..=at_most.floor() as u32 {
+    for hold in at_least.ceil() as u64..=at_most.floor() as u64 {
         if hold * (race_time - hold) > *min_distance {
-            found.push(hold);
+            found += 1;
         }
     }
 
