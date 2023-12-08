ecc3316a6d2775605f8018638e6d15d24322150f
harphield <harphield@gmail.com>
2023-12-08 09:54:56 +0100

    Day 8 part 2 naive

--- a08/src/main.rs
+++ a08/src/main.rs
@@ -4,12 +4,14 @@ use std::fs::File;
 use std::io;
 use std::io::BufRead;
 
+type Nodes = HashMap<String, (String, String)>;
+
 fn main() -> Result<(), io::Error> {
     let file = File::open("input.txt")?;
 
     let mut directions = vec![];
-    let mut nodes = HashMap::new();
-    let re = Regex::new("([A-Z]+)").unwrap();
+    let mut nodes = Nodes::new();
+    let re = Regex::new("([0-9A-Z]+)").unwrap();
 
     for (n, line_r) in io::BufReader::new(file).lines().enumerate() {
         match line_r {
@@ -30,8 +32,8 @@ fn main() -> Result<(), io::Error> {
         }
     }
 
+    // Part 1
     let mut current_node = "AAA".to_string();
-
     let mut dir_index = 0usize;
     let directions_length = directions.len();
     let mut steps = 0;
@@ -53,5 +55,57 @@ fn main() -> Result<(), io::Error> {
 
     println!("Part 1 result: {}", steps);
 
+    // Part 2
+    let mut current_nodes = find_ending_with_a(&nodes);
+
+    println!("{:?}", current_nodes);
+
+    dir_index = 0;
+    steps = 0;
+
+    while !do_all_end_in_z(&current_nodes) {
+        if dir_index >= directions_length {
+            dir_index = 0;
+        }
+
+        let mut new_current_nodes = current_nodes.clone();
+
+        for (i, current_node) in current_nodes.iter().enumerate() {
+            let cn = nodes.get(current_node).unwrap();
+            new_current_nodes[i] = match directions[dir_index] {
+                'L' => cn.0.to_string(),
+                'R' => cn.1.to_string(),
+                _ => panic!("wrong dir"),
+            };
+        }
+
+        current_nodes = new_current_nodes;
+
+        // println!("{:?}", current_nodes);
+
+        steps += 1;
+        dir_index += 1;
+    }
+
+    println!("Part 2 result: {}", steps);
+
     Ok(())
+}
+
+fn find_ending_with_a(nodes: &Nodes) -> Vec<String> {
+    let mut result = vec![];
+
+    for (k, _v) in nodes.iter() {
+        if k.ends_with('A') {
+            result.push(k.clone());
+        }
+    }
+
+    result
+}
+
+fn do_all_end_in_z(nodes: &[String]) -> bool {
+    nodes.iter().all(|v| {
+        v.ends_with('Z')
+    })
 }
