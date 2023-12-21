172b6b6f74abc7db945befa978730ac057941b6d
harphield <harphield@gmail.com>
2023-12-21 01:08:12 +0100

    Day 20 prep

--- a20/src/main.rs
+++ a20/src/main.rs
@@ -3,14 +3,14 @@ use std::fs::File;
 use std::io;
 use std::io::BufRead;
 
+#[derive(Clone, Debug)]
 enum Pulse {
     Low(String),
     High(String),
 }
 
-trait Module<T> {
-    fn create(name: &String, destinations: Vec<String>) -> T;
-    fn accept_pulse(&mut self, pulse: Pulse) -> Option<(Pulse, Vec<String>)>;
+trait Module {
+    fn accept_pulse(&mut self, pulse: &Pulse) -> Option<(Pulse, Vec<String>)>;
 }
 
 struct FlipFlop {
@@ -19,7 +19,7 @@ struct FlipFlop {
     destinations: Vec<String>,
 }
 
-impl Module<FlipFlop> for FlipFlop {
+impl FlipFlop {
     fn create(name: &String, destinations: Vec<String>) -> FlipFlop {
         FlipFlop {
             name: name.clone(),
@@ -27,14 +27,15 @@ impl Module<FlipFlop> for FlipFlop {
             destinations,
         }
     }
-
-    fn accept_pulse(&mut self, pulse: Pulse) -> Option<(Pulse, Vec<String>)> {
+}
+impl Module for FlipFlop {
+    fn accept_pulse(&mut self, pulse: &Pulse) -> Option<(Pulse, Vec<String>)> {
         match pulse {
             Pulse::Low(_) => {
                 self.state = !self.state;
 
                 Some((
-                    if self.state { Pulse::High(self.name.clone()) } else { Pulse::Low },
+                    if self.state { Pulse::High(self.name.clone()) } else { Pulse::Low(self.name.clone()) },
                     self.destinations.clone(),
                 ))
             }
@@ -44,18 +45,125 @@ impl Module<FlipFlop> for FlipFlop {
 }
 
 struct Conjunction {
-    inputs: HashMap<String, Pulse>
+    name: String,
+    inputs: HashMap<String, Pulse>,
+    destinations: Vec<String>,
+}
+
+impl Conjunction {
+    fn create(name: &String, destinations: Vec<String>) -> Conjunction {
+        Conjunction {
+            name: name.clone(),
+            inputs: HashMap::new(),
+            destinations,
+        }
+    }
+}
+
+impl Module for Conjunction {
+    fn accept_pulse(&mut self, pulse: &Pulse) -> Option<(Pulse, Vec<String>)> {
+        let p = pulse.clone();
+        match pulse {
+            Pulse::Low(from) | Pulse::High(from) => {
+                let f = from.clone();
+                *self.inputs.entry(f.clone()).or_insert(Pulse::Low(f.clone())) = p;
+            }
+        }
+
+        if self.inputs.iter().all(|(_k, v)| {
+            match v {
+                Pulse::Low(_) => false,
+                Pulse::High(_) => true
+            }
+        }) {
+            Some((Pulse::High(self.name.clone()), self.destinations.clone()))
+        } else {
+            Some((Pulse::Low(self.name.clone()), self.destinations.clone()))
+        }
+    }
+}
+
+struct Broadcast {
+    name: String,
+    destinations: Vec<String>,
+}
+
+impl Broadcast {
+    fn create(name: &String, destinations: Vec<String>) -> Broadcast {
+        Broadcast {
+            name: name.clone(),
+            destinations,
+        }
+    }
+}
+
+impl Module for Broadcast {
+    fn accept_pulse(&mut self, pulse: &Pulse) -> Option<(Pulse, Vec<String>)> {
+        Some((pulse.clone(), self.destinations.clone()))
+    }
 }
 
 fn main() -> Result<(), io::Error> {
     let file = File::open("input.txt")?;
 
+    let mut config: HashMap<String, Box<dyn Module>> = HashMap::new();
+
     for line_r in io::BufReader::new(file).lines() {
         match line_r {
-            Ok(line) => {}
+            Ok(line) => {
+                let s = line.split(" -> ").collect::<Vec<&str>>();
+                let destinations = s[1].split(", ").map(|v| v.to_string()).collect();
+
+                if s[0] == "broadcaster" {
+                    config.insert(s[0].to_string(), Box::new(Broadcast::create(&s[0].to_string(), destinations)));
+                } else if s[0].starts_with('%') {
+                    // flipflop
+                    let name = &s[0][1..];
+                    config.insert(name.to_string(), Box::new(FlipFlop::create(&name.to_string(), destinations)));
+                } else if s[0].starts_with('&') {
+                    let name = &s[0][1..];
+                    config.insert(name.to_string(), Box::new(Conjunction::create(&name.to_string(), destinations)));
+                }
+            }
             Err(_) => break,
         }
     }
+
+    config.iter().for_each(|(k, v)| {
+        println!("{}", k);
+    });
+
+    let mut queue = vec![(Pulse::Low("".to_string()), "broadcaster".to_string())];
+
+    let mut low_pulses = 1;  // the default push
+    let mut high_pulses = 0;
+    loop {
+        if queue.is_empty() {
+            break;
+        }
+
+        let (pulse, destination) = queue.pop().unwrap();
+        match config.get_mut(&destination) {
+            None => {}
+            Some(module) => {
+                match module.accept_pulse(&pulse) {
+                    None => {}
+                    Some((r_pulse, r_destinations)) => {
+                        for d in r_destinations.iter() {
+                            queue.insert(0, (r_pulse.clone(), d.clone()));
+
+                            match r_pulse {
+                                Pulse::Low(_) => { low_pulses += 1; }
+                                Pulse::High(_) => { high_pulses += 1; }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    println!("{} high {} low", high_pulses, low_pulses);
 
     Ok(())
 }
